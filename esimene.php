35fdcc2252c177d32b0c3c844ddb892815afa095
Rauno Piibor <raunopiibor123@users.noreply.github.com>
2017-09-14 10:46:55 +0300

    Lisatud on esimene php jupp

---
+++ esimene.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Rauno Piibor progemise asjad</title>
+
+</head>
+<body>
+	<h1>Rauno Piibor</h1>
+	<p>See veebileht on loodud veebiprogrammeerimise kursusel ning ei sisalda mingisugust tõsiseltvõetavat sisu. jne</p>	
+	<?php
+	echo "Algas PHP õppimine.";
+	?>
+</body>
+</html>
