622645c97cab93ef17ff3cda80e8d461136b6091
Rauno Piibor <raunopiibor123@users.noreply.github.com>
2017-09-21 11:33:07 +0300

    Lisatud pildid

---
+++ Tund3/avaleht.php
@@ -0,0 +1,31 @@
+<?php
+	//muutujad
+	$myName = "Rauno";
+	$myFamilyName = "Piibor";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>
+		Rauno Piibor progemise asjad</title>
+
+</head>
+<body>
+	<h1><?php echo $myName ." ".$myFamilyName;?></h1>
+	<p>See veebileht on loodud veebiprogrammeerimise kursusel ning ei sisalda mingisugust tõsiseltvõetavat sisu. jne</p>	
+	<p>Meie õpime <a href="https://www.tlu.ee/">Tallinna Ülikoolis</a>.</p>
+	<p>Minu esimene php leht on <a href="../esimene.php">siin</a>.</p>
+	<p>Minu sõbra Meelise leht on <a href="../../../~lutsmeel/Veebiprogrammeerimine">siin</a> </p>
+	<p>Pilte näeb <a href="foto.php">siin</a> .</p>
+</body>
+</html>
+
+
+
+
+
+
+
+
